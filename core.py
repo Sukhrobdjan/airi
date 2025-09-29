8a703881e45f63d8babfbd0bdeb89a69d15ba123
suxrob <suxidev@gmail.com>
2025-09-29 11:46:24 +0500

    core problems

---
+++ core.py
@@ -0,0 +1,14 @@
+name = "Hello world"
+print(name[1])
+print(name[2:5])
+print(name[-1]) # last character d
+print(name[-2]) # second last character l
+# +---+---+---+---+---+---+---+---+---+---+---+---+---+
+# | P | y | t | h | o | n |   | S | t | r | i | n | g | 
+# +---+---+---+---+---+---+---+---+---+---+---+---+---+
+#   0   1   2   3   4   5   6   7   8   9   10  11  12
+# -13  -12  -11  -10 -9  -8  -7  -6  -5  -4  -3  -2  -
+name_len = len(name)
+print(name_len)
+gap = name[3:8]
+print(gap)
